2a5d4c087c65c1752f9166959b21fb6e1c44d189
Luke Merrett <lukeamerrett@gmail.com>
2017-11-17 18:01:59 +0000

    Method to get selected option from user

--- user/user.go
+++ user/user.go
@@ -1,9 +1,12 @@
 package user
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"github.com/lukemerrett/go-explore/model"
+	"os"
+	"strconv"
 )
 
 // OutputScene takes a scene and builds the string to show to the user
@@ -18,6 +21,32 @@ func OutputScene(scene model.Scene) string {
 		}
 	}
 	return buffer.String()
+}
+
+// GetNextScene waits for the user to select the next scene from the options
+func GetNextScene(currentScene model.Scene) model.Scene {
+	optionCount := int64(0)
+	if optionPresent(currentScene.Options) {
+		optionCount = int64(len(currentScene.Options))
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("\n\nPlease choose an option (1-%v):\n", optionCount)
+
+	selectedOption := 1
+
+	for true {
+		text, _ := reader.ReadString('\n')
+		selectedOption, err := strconv.ParseInt(text, 10, 64)
+
+		if err != nil || selectedOption > optionCount || selectedOption < optionCount {
+			fmt.Printf("\n\nPlease enter a number between 1-%v:\n", optionCount)
+		} else {
+			break
+		}
+	}
+
+	return currentScene.Options[selectedOption-1].Scene
 }
 
 func optionPresent(list []model.Trigger) bool {
