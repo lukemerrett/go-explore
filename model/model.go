dc9ff46e0c29d6e642bca3fd6be21b902118fe51
Luke Merrett <lukeamerrett@gmail.com>
2017-11-20 12:08:37 +0000

    Added yaml mapping to model

--- model/model.go
+++ model/model.go
@@ -2,12 +2,12 @@ package model
 
 // GameData holds a list of the scenes available within a game
 type GameData struct {
-	Scenes map[string]Scene
+	Scenes map[string]Scene `yaml:"Scenes"`
 }
 
 // Scene holds the information to show the user about a particular scene and what they can do next
 type Scene struct {
-	Title       string
-	Body        string
-	Transitions map[string]string
+	Title       string            `yaml:"Title"`
+	Body        string            `yaml:"Body"`
+	Transitions map[string]string `yaml:"Transitions"`
 }
