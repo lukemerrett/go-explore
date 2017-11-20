a9018b0b42df0722822adb686a072784eeaab687
Luke Merrett <lukeamerrett@gmail.com>
2017-11-20 13:52:25 +0000

    Handled output type in main func

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/lukemerrett/go-explore/config"
 	"github.com/lukemerrett/go-explore/format"
 	"github.com/lukemerrett/go-explore/game"
+	"github.com/lukemerrett/go-explore/model"
+	"strings"
 )
 
 const (
@@ -15,9 +17,22 @@ func main() {
 	gameData, err := config.LoadFromYaml(configFile)
 	handleError(err)
 
-	formatter := format.ConsoleFormatter{}
+	formatter, err := getDependencies(gameData)
+	handleError(err)
+
 	err = game.RunGame(gameData, formatter)
 	handleError(err)
+}
+
+func getDependencies(gameData model.GameData) (format.Formatter, error) {
+	outputType := strings.ToLower(gameData.Configuration.OutputType)
+
+	switch outputType {
+	case "console":
+		return format.ConsoleFormatter{}, nil
+	}
+
+	return nil, fmt.Errorf("Could not find matching output type for %v", outputType)
 }
 
 func handleError(err error) {
