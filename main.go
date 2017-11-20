16ea27ca3b4b591893e7e530f1e1fb866829840a
Luke Merrett <lukeamerrett@gmail.com>
2017-11-20 12:25:39 +0000

    Hooked up main code to test framework

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/lukemerrett/go-explore/config"
+	"github.com/lukemerrett/go-explore/game"
+)
+
+const (
+	configFile string = "resources/game-data.yml"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	gameData, err := config.LoadFromYaml(configFile)
+	handleError(err)
+
+	err = game.RunGame(gameData)
+	handleError(err)
+}
+
+func handleError(err error) {
+	if err != nil {
+		fmt.Printf("%v", err)
+		panic(err)
+	}
 }
