3d585e8517bcbffa9f6b98334993d6da485dd43e
Luke Merrett <lukeamerrett@gmail.com>
2017-11-20 11:23:53 +0000

    Updated model to store scenes in a map

--- model/model.go
+++ model/model.go
@@ -1,14 +1,19 @@
 package model
 
-// Trigger maps between an option a user can choose and the next scene they will see
-type Trigger struct {
-	Option string
-	Scene  Scene
+// GameData holds a list of the scenes available within a game
+type GameData struct {
+	Scenes map[string]Scene
 }
 
 // Scene holds the information to show the user about a particular scene and what they can do next
 type Scene struct {
-	Title   string
-	Body    string
-	Options []Trigger
+	Title       string
+	Body        string
+	Transitions []Transition
+}
+
+// Transition maps between an option a user can choose and the next scene they will see
+type Transition struct {
+	Option   string
+	SceneKey string
 }
